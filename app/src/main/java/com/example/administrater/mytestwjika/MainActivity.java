49d71c151deaa05047ad458ec0d9d25737ab76dd
zhaoweiwei <zhaoweiwei@wjika.com>
2016-06-02 10:38:33 +0800

    editText代码中设置文字并不可点击

--- app/src/main/java/com/example/administrater/mytestwjika/MainActivity.java
+++ app/src/main/java/com/example/administrater/mytestwjika/MainActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
 	private Button mButtonNotification;//权限
-	private TextView mTextViewLoca;
+	private TextView mTextViewLoca;//在代码中改变textview中文字的位置gravity
+	private EditText mEditSetWords;//在代码中给输入框设置文字
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,6 +22,9 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		mButtonNotification.setOnClickListener(this);
 		mTextViewLoca = (TextView) findViewById(R.id.text_address);
 		mTextViewLoca.setOnClickListener(this);
+		mEditSetWords = (EditText) findViewById(R.id.edit_settext);
+		mEditSetWords.setText("zheshizaiwenbenzhongshezhidewenzi");
+		mEditSetWords.setEnabled(false);
 	}
 
 	@Override
